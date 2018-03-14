36b6c916e54b0b990697d43cbd9899a1cea3868c
Jamison64 <jzshah@thecurve.com.au>
2018-03-14 20:29:37 +1100

    updating

---
+++ App-Swoosh/App-Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  App-Swoosh
+//
+//  Created by Jamison64 on 6/3/18.
+//  Copyright © 2018 theCurve. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkilledLevel: String?
+}
